4a1342421c28b69c1ca4fa099a76fb98403cbdd6
Dmitriy Ivanov <dmitriy.ivanov@travelline.ru>
2024-04-20 15:22:58 +0300

    fix: Fixed operations priority

--- FiniteAutomatas.RegularExpressions/Implementation/Models/RegexNode.cs
+++ FiniteAutomatas.RegularExpressions/Implementation/Models/RegexNode.cs
@@ -15,49 +15,18 @@ internal class RegexNode
         {
             throw new Exception();
         }
-
-        expression = SimplifyBracesIfNeed( expression );
-        if ( expression.Length == 1 )
-        {
-            expression = $"{expression}|{expression}";
-        }
         
-        return new RegexNode( RegexSymbol.Parse( expression ), null );
+        return new RegexNode( 
+            RegexSymbol.Parse( expression.Length == 1 ? $"{expression}|{expression}" : expression ),
+            null );
     }
     
     private RegexNode( List<RegexSymbol> regex, RegexNode? parent )
     {
         _parent = parent;
-        regex = SimplifyBracesIfNeed( regex );
+        regex = regex.SimplifyBracesIfNeed();
 
-        int symbolIndex = -1;
-        var braces = new Queue<RegexSymbolType>();
-        for ( int i = 0; i < regex.Count; i++ )
-        {
-            if ( regex[i].Type == RegexSymbolType.OpenBrace )
-            {
-                braces.Enqueue( regex[i].Type );
-                continue;
-            }
-
-            if ( regex[i].Type == RegexSymbolType.CloseBrace )
-            {
-                braces.Dequeue();
-                continue;
-            }
-
-            bool wedgeFound =
-                !braces.Any()
-                && regex[i].Type is 
-                    not RegexSymbolType.Symbol and
-                    not RegexSymbolType.OneOrMore and
-                    not RegexSymbolType.ZeroOrMore; 
-            if ( wedgeFound )
-            {
-                symbolIndex = i;
-                break;
-            } 
-        }
+        int symbolIndex = regex.GetOperationSymbolIndex();
 
         // No operators were found
         if ( symbolIndex == -1 )
@@ -78,12 +47,11 @@ internal class RegexNode
             return;
         }
 
-        Value = regex[symbolIndex];
-        
         var left = regex.GetRange( 0, symbolIndex );
-        LeftOperand = left.Count > 0 ? new RegexNode( left, this ) : null;
-        
         var right = regex.GetRange( symbolIndex + 1, regex.Count - symbolIndex - 1 );
+        
+        Value = regex[symbolIndex];
+        LeftOperand = left.Count > 0 ? new RegexNode( left, this ) : null;
         RightOperand = right.Count > 0 ? new RegexNode( right, this ) : null;
     }
 
@@ -110,81 +78,4 @@ internal class RegexNode
     }
 
     public override string ToString() => $"(V={Value}, L={LeftOperand}, R={RightOperand})";
-
-    private static string SimplifyBracesIfNeed( string expression )
-    {
-        while ( expression.First() == '(' && expression.Last() == ')' )
-        {
-            bool needToRemove = true;
-            
-            int bracesLevel = 0;
-            for ( int i = 0; i < expression.Length && needToRemove; i++ )
-            {
-                if ( expression[i] == '(' )
-                {
-                    bracesLevel += 1;
-                    continue;
-                }
-
-                if ( expression[i] == ')' )
-                {
-                    bracesLevel -= 1;
-                    if ( bracesLevel == 0 && i + 1 < expression.Length - 1 )
-                    {
-                        needToRemove = false;
-                    }
-                }
-            }
-
-            if ( needToRemove )
-            {
-                expression = expression.Substring( 1, expression.Length - 2 );
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return expression;
-    }
-
-    private static List<RegexSymbol> SimplifyBracesIfNeed( List<RegexSymbol> regex )
-    {
-        while ( regex.First().Type == RegexSymbolType.OpenBrace && regex.Last().Type == RegexSymbolType.CloseBrace )
-        {
-            bool needToRemove = true;
-            
-            int bracesLevel = 0;
-            for ( int i = 0; i < regex.Count && needToRemove; i++ )
-            {
-                if ( regex[i].Type == RegexSymbolType.OpenBrace )
-                {
-                    bracesLevel += 1;
-                    continue;
-                }
-
-                if ( regex[i].Type == RegexSymbolType.CloseBrace )
-                {
-                    bracesLevel -= 1;
-                    if ( bracesLevel == 0 && i + 1 < regex.Count - 1 )
-                    {
-                        needToRemove = false;
-                    }
-                }
-            }
-
-            if ( needToRemove )
-            {
-                regex.RemoveAt( 0 );
-                regex.RemoveAt( regex.Count - 1 );
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return regex;
-    }
 }
---
+++ FiniteAutomatas.RegularExpressions/Implementation/Models/RegexSymbolListExtensions.cs
@@ -0,0 +1,86 @@
+﻿namespace FiniteAutomatas.RegularExpressions.Implementation.Models;
+
+internal static class RegexSymbolListExtensions
+{
+    private static readonly Dictionary<RegexSymbolType, int> _symbolTypeToPriority = new()
+    {
+        { RegexSymbolType.Symbol, 0 },
+        { RegexSymbolType.OpenBrace, 0 },
+        { RegexSymbolType.CloseBrace, 0 },
+        { RegexSymbolType.And, 3 },
+        { RegexSymbolType.Or, 2 },
+        { RegexSymbolType.ZeroOrMore, 1 },
+        { RegexSymbolType.OneOrMore, 1 },
+    };
+
+    public static int GetOperationSymbolIndex( this List<RegexSymbol> regex )
+    {
+        var symbolIndexToPriority = new Dictionary<int, int>();
+
+        int bracesLevel = 0;
+        for ( int i = 0; i < regex.Count; i++ )
+        {
+            RegexSymbol symbol = regex[i];
+
+            if ( symbol.Type == RegexSymbolType.OpenBrace )
+            {
+                bracesLevel++;
+                symbolIndexToPriority[i] = _symbolTypeToPriority[symbol.Type];
+                continue;
+            }
+
+            if ( symbol.Type == RegexSymbolType.CloseBrace )
+            {
+                bracesLevel--;
+                symbolIndexToPriority[i] = _symbolTypeToPriority[symbol.Type];
+                continue;
+            }
+            
+            symbolIndexToPriority[i] = bracesLevel == 0 
+                ? _symbolTypeToPriority[symbol.Type] 
+                : 0;
+        }
+
+        return symbolIndexToPriority.MaxBy( x => x.Value ).Key;
+    }
+    
+    public static List<RegexSymbol> SimplifyBracesIfNeed( this List<RegexSymbol> regex )
+    {
+        while ( regex.First().Type == RegexSymbolType.OpenBrace && 
+                regex.Last().Type == RegexSymbolType.CloseBrace )
+        {
+            bool needToRemove = true;
+            
+            int bracesLevel = 0;
+            for ( int i = 0; i < regex.Count && needToRemove; i++ )
+            {
+                if ( regex[i].Type == RegexSymbolType.OpenBrace )
+                {
+                    bracesLevel += 1;
+                    continue;
+                }
+
+                if ( regex[i].Type == RegexSymbolType.CloseBrace )
+                {
+                    bracesLevel -= 1;
+                    if ( bracesLevel == 0 && i + 1 < regex.Count - 1 )
+                    {
+                        needToRemove = false;
+                    }
+                }
+            }
+
+            if ( needToRemove )
+            {
+                regex.RemoveAt( 0 );
+                regex.RemoveAt( regex.Count - 1 );
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return regex;
+    }
+}
