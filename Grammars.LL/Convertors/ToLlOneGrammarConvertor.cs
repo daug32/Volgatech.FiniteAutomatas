283b7b9ade662d123d4f60d9a7a438f5d7db5f22
Dmitriy Ivanov <dmitriy.ivanov@travelline.ru>
2024-05-30 18:01:30 +0300

    .

--- Grammars.LL/Convertors/ToLlOneGrammarConvertor.cs
+++ Grammars.LL/Convertors/ToLlOneGrammarConvertor.cs
@@ -29,8 +29,6 @@ public class ToLlOneGrammarConvertor : IGrammarConvertor<LlOneGrammar>
             .Convert( new RemoveEpsilonsConvertor() )
             .Convert( new RenameRuleNamesConvertor() );
 
-        ValidateAmbiguousRuleDefinitionsOrThrow( normalizedGrammar );
-
         SanitizeEndSymbols( normalizedGrammar );
 
         return new LlOneGrammar( 
@@ -57,79 +55,6 @@ public class ToLlOneGrammarConvertor : IGrammarConvertor<LlOneGrammar>
         grammar.Rules[grammar.StartRule].Definitions = grammar.Rules[grammar.StartRule]
             .Definitions
             .Select( definition => new RuleDefinition( definition.Symbols.Append( RuleSymbol.TerminalSymbol( TerminalSymbol.End() ) ) ) )
-            .ToList();
-    }
-
-    private void ValidateAmbiguousRuleDefinitionsOrThrow( CommonGrammar grammar )
-    {
-        foreach ( GrammarRule rule in grammar.Rules.Values )
-        {
-            List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> headingSymbols = rule.Definitions
-                .Select( definition => 
-                    (
-                        Definition: definition,
-                        FirstSet: grammar.GetFirstSet( rule.Name, definition ).GuidingSymbols
-                    ) )
-                .ToList();
-
-            List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> duplicateHeadingSets = FindDuplicateHeadingSets( headingSymbols );
-
-            if ( duplicateHeadingSets.Any() )
-            {
-                string serializedDefinitions = String.Join(
-                    "\n",
-                    duplicateHeadingSets.Select( definition =>
-                    {
-                        string serializedDefinition = String.Join( " ", definition.Definition.Symbols );
-                        string serializedSet = String.Join( ",", definition.FirstSet );
-                        return $"<{rule.Name}> -> {serializedDefinition}\tFirstSet: {serializedSet}";
-                    } ) );
-                
-                throw new ArgumentException(
-                    $"Rule {rule.Name} has duplicate FIRST for different definitions\n{serializedDefinitions}" );
-            }
-        }
-    }
-
-    private static List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> FindDuplicateHeadingSets(
-        List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> definitionsWithFirstSet )
-    {
-        var result = new List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)>();
-
-        for ( var mainIterator = 0; mainIterator < definitionsWithFirstSet.Count; mainIterator++ )
-        {
-            var mainDefinition = definitionsWithFirstSet[mainIterator];
-
-            var duplicates = new List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)>()
-            {
-                mainDefinition
-            };
-            
-            for ( int secondIterator = mainIterator + 1; secondIterator < definitionsWithFirstSet.Count; secondIterator++ )
-            {
-                var secondDefinition = definitionsWithFirstSet[secondIterator];
-                if ( secondDefinition.FirstSet.SetEquals( mainDefinition.FirstSet ) )
-                {
-                    duplicates.Add( secondDefinition );
-                    definitionsWithFirstSet.RemoveAt( secondIterator );
-                    secondIterator--;
-                }
-            }
-
-            if ( duplicates.Count > 1 )
-            {
-                result.AddRange( duplicates );
-                definitionsWithFirstSet.RemoveAt( mainIterator );
-                mainIterator--;
-            }
-        }
-
-        return result
-            .OrderBy( x => String.Join( 
-                "", 
-                x.FirstSet
-                    .OrderBy( i => i.ToString() )
-                    .Select( i => i.ToString() ) ) )
             .ToList();
     }
 }
--- Grammars.LL/Models/LlOneGrammar.cs
+++ Grammars.LL/Models/LlOneGrammar.cs
@@ -1,6 +1,10 @@
-﻿using Grammars.Common.Grammars;
+﻿using System.Collections.Immutable;
+using Grammars.Common.Grammars;
+using Grammars.Common.Grammars.Extensions;
 using Grammars.Common.Grammars.ValueObjects;
 using Grammars.Common.Grammars.ValueObjects.GrammarRules;
+using Grammars.Common.Grammars.ValueObjects.RuleDefinitions;
+using Grammars.Common.Grammars.ValueObjects.Symbols;
 using Grammars.LL.Runners;
 using Grammars.LL.Runners.Results;
 
@@ -11,6 +15,7 @@ public class LlOneGrammar : CommonGrammar
     public LlOneGrammar( RuleName startRule, IEnumerable<GrammarRule> rules )
         : base( startRule, rules )
     {
+        ValidateAmbiguousRuleDefinitionsOrThrow( this );
     }
 
     public RunResult Run( string sentence )
@@ -23,5 +28,78 @@ public class LlOneGrammar : CommonGrammar
         {
             return RunResult.Fail( sentence, RunError.NotLl() );
         }
+    }
+
+    private void ValidateAmbiguousRuleDefinitionsOrThrow( CommonGrammar grammar )
+    {
+        foreach ( GrammarRule rule in grammar.Rules.Values )
+        {
+            List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> headingSymbols = rule.Definitions
+                .Select( definition => 
+                    (
+                        Definition: definition,
+                        FirstSet: grammar.GetFirstSet( rule.Name, definition ).GuidingSymbols
+                    ) )
+                .ToList();
+
+            List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> duplicateHeadingSets = FindDuplicateHeadingSets( headingSymbols );
+
+            if ( duplicateHeadingSets.Any() )
+            {
+                string serializedDefinitions = String.Join(
+                    "\n",
+                    duplicateHeadingSets.Select( definition =>
+                    {
+                        string serializedDefinition = String.Join( " ", definition.Definition.Symbols );
+                        string serializedSet = String.Join( ",", definition.FirstSet );
+                        return $"<{rule.Name}> -> {serializedDefinition}\tFirstSet: {serializedSet}";
+                    } ) );
+                
+                throw new ArgumentException(
+                    $"Rule {rule.Name} has duplicate FIRST for different definitions\n{serializedDefinitions}" );
+            }
+        }
+    }
+
+    private static List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> FindDuplicateHeadingSets(
+        List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)> definitionsWithFirstSet )
+    {
+        var result = new List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)>();
+
+        for ( var mainIterator = 0; mainIterator < definitionsWithFirstSet.Count; mainIterator++ )
+        {
+            var mainDefinition = definitionsWithFirstSet[mainIterator];
+
+            var duplicates = new List<(RuleDefinition Definition, IImmutableSet<RuleSymbol> FirstSet)>()
+            {
+                mainDefinition
+            };
+            
+            for ( int secondIterator = mainIterator + 1; secondIterator < definitionsWithFirstSet.Count; secondIterator++ )
+            {
+                var secondDefinition = definitionsWithFirstSet[secondIterator];
+                if ( secondDefinition.FirstSet.SetEquals( mainDefinition.FirstSet ) )
+                {
+                    duplicates.Add( secondDefinition );
+                    definitionsWithFirstSet.RemoveAt( secondIterator );
+                    secondIterator--;
+                }
+            }
+
+            if ( duplicates.Count > 1 )
+            {
+                result.AddRange( duplicates );
+                definitionsWithFirstSet.RemoveAt( mainIterator );
+                mainIterator--;
+            }
+        }
+
+        return result
+            .OrderBy( x => String.Join( 
+                "", 
+                x.FirstSet
+                    .OrderBy( i => i.ToString() )
+                    .Select( i => i.ToString() ) ) )
+            .ToList();
     }
 }
