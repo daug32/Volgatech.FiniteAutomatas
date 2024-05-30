ae78be6cbae2266c099f74c4ff6847782fb3b748
Dmitriy Ivanov <dmitriy.ivanov@travelline.ru>
2024-05-30 18:04:15 +0300

    .

--- Grammars.Common.Convertors/Convertors/RemoveWhitespacesConvertor.cs
+++ Grammars.Common.Convertors/Convertors/RemoveWhitespacesConvertor.cs
@@ -1,7 +1,4 @@
 ﻿using Grammars.Common.Grammars;
-using Grammars.Common.Grammars.ValueObjects.GrammarRules;
-using Grammars.Common.Grammars.ValueObjects.RuleDefinitions;
-using Grammars.Common.Grammars.ValueObjects.Symbols;
 
 namespace Grammars.Common.Convertors.Convertors;
 
@@ -9,38 +6,6 @@ public class RemoveWhitespacesConvertor : IGrammarConvertor
 {
     public CommonGrammar Convert( CommonGrammar grammar )
     {
-        var rules = new List<GrammarRule>();
-        foreach ( GrammarRule rule in grammar.Rules.Values )
-        {
-            var definitions = new List<RuleDefinition>();
-            foreach ( RuleDefinition definition in rule.Definitions )
-            {
-                var symbols = new List<RuleSymbol>();
-                foreach ( RuleSymbol symbol in definition.Symbols )
-                {
-                    if ( symbol.Type == RuleSymbolType.TerminalSymbol )
-                    {
-                        if ( symbol.Symbol!.Type == TerminalSymbolType.WhiteSpace )
-                        {
-                            continue;
-                        }
-
-                        symbols.Add( symbol );
-                        continue;
-                    }
-
-                    symbols.Add( symbol );
-                }
-
-                if ( symbols.Any() )
-                {
-                    definitions.Add( new RuleDefinition( symbols ) );
-                }
-            }
-
-            rules.Add( new GrammarRule( rule.Name, definitions ) );
-        }
-
-        return new CommonGrammar( grammar.StartRule, rules );
+        return new WhitespacesRemover().RemoveWhitespaces( grammar );
     }
 }
